18a3c81d0283bd288f5e7e040dc540d2af9f9255
simonare <simonarez@hotmail.com>
2024-02-09 00:22:00 +0200

    Typescript 5, decorator error fix

--- src/lib/aws-service.decorator.ts
+++ src/lib/aws-service.decorator.ts
@@ -2,7 +2,9 @@ import { Inject } from '@nestjs/common';
 import { getAwsServiceToken } from './tokens';
 import { AwsService, AwsServiceType } from './types';
 
-export const InjectAwsService = (
+export const InjectAwsService: (
+  serviceConstructor: AwsServiceType<AwsService>,
+) => PropertyDecorator & ParameterDecorator = (
   serviceConstructor: AwsServiceType<AwsService>,
 ) => {
   return Inject(getAwsServiceToken(serviceConstructor));
